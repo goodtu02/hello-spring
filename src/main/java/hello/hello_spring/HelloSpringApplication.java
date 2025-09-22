10ae3fafb80e2452a9e7b7f36a12161669cca63c
권태욱 <notebook@>
2025-09-22 19:35:16 +0900

    09/22

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -3,11 +3,20 @@ package hello.hello_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class HelloSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+
+    public void add(){
+        Scanner sc = new Scanner(System.in);
+        Integer k  = sc.nextInt();
+        String ks = sc.next();
+
+    }
 
 }
